a92c4a5d3ada8b725387d2be0accc7ac968c0d98
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-05 17:21:33 +0100

    add more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import netlify from "@astrojs/netlify";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-  site: "",
-  // sitemap()
+  site: 'https://yourdomain.com',
+  output: "server",
+  adapter: netlify(),
+  integrations: [sitemap()],
 });
