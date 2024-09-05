c6d7fc6ebe42d084c94bded8aae01009318cd728
pawelkom88 <pawel.komorkiewicz@yahoo.co.uk>
2024-09-05 08:11:10 +0100

    chore: basic reset

---
+++ site.config.ts
@@ -0,0 +1,15 @@
+interface SiteConfig {
+  author: string;
+  title: string;
+  description: string;
+  lang: string;
+  ogLocale: string;
+}
+
+export const siteConfig: SiteConfig = {
+  author: "Pawel Komorkiewicz", // Site author
+  title: "Ludek", // Site title.
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", // Description to display in the meta tags
+  lang: "en-GB",
+  ogLocale: "en_GB",
+};
