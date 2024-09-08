6ee974de24d1a03ab099d2548d25f0f27324dbb7
ludekwebsite <ludekwebsite@gmail.com>
2024-09-08 13:26:57 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
-import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yourdomain.com',
+  site: "https://yourdomain.com",
   output: "server",
   adapter: netlify(),
   integrations: [sitemap()],
+  image: {
+    domains: [
+      "https://csimg.nyc3.cdn.digitaloceanspaces.com,https://scontent-lhr6-2.xx.fbcdn.net,https://scontent-lhr8-1.xx.fbcdn.net",
+    ],
+  },
 });
